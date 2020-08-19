d5a40b494db142d978be086a5c685f4b48e6dded
Chris Tsui <chris.tsui112@outlook.com>
2020-08-19 05:33:37 -0700

    refactored

--- code.js
+++ code.js
@@ -1,8 +1,7 @@
-const inputNodeList = document.querySelectorAll("input");
-inputNodeList[1].addEventListener('click',()=>{
-    const userInput = inputNodeList[0].value;
-    inputNodeList[0].value = "";
-    const ul = document.getElementById("ul");
+function createListItems(textField){
+    const userInput = textField.value;
+    textField.value = "";
+    let ul = document.getElementById("ul");
     let li = document.createElement("li");
     li.textContent = userInput;
     li.classList.add("li","completed");
@@ -10,14 +9,43 @@ inputNodeList[1].addEventListener('click',()=>{
     li.addEventListener("click", ()=>{
         li.classList.toggle("completed");
     })
+    addDeleteButton(ul,li);
+}
+function addDeleteButton(ul,li) {
     let deleteButton = document.createElement("button");
     deleteButton.setAttribute("value", "delete");
     deleteButton.setAttribute("style", "height:14px");
-    deleteButton.addEventListener("click", event =>{
-        const target = event.target;
-        const parentNode = target.parentNode;
-        parentNode.remove();
-    })
     li.appendChild(deleteButton);
     ul.appendChild(li);
-})
+    deleteButton.addEventListener("click", event => {
+        let target = event.target;
+        let parentNode = target.parentNode;
+        parentNode.remove();
+    });
+}
+function attachEventListener() {
+    const inputNodeList = document.querySelectorAll("input");
+    inputNodeList[1].addEventListener('click', event => {
+        createListItems(inputNodeList[0]);
+    })
+}
+window.onload= init;
+
+function init(){
+    attachEventListener();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
